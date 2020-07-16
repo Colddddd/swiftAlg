668cb3e8f3caebd05bbb1d5952a3c4ff380b1293
lt <>
2020-07-16 17:14:08 +0800

    feat: 5 leetcode

--- suanfa.playground/Pages/5. 最长回文子串 .xcplaygroundpage/Contents.swift
+++ suanfa.playground/Pages/5. 最长回文子串 .xcplaygroundpage/Contents.swift
@@ -17,10 +17,75 @@ var str = "Hello, playground"
  
  输入: "cbbd"
  输出: "bb"
+ 
+ 
+奇： a  aba abcba
+ 
+偶： aa abba
+ 
  */
-func longestPalindrome(_ s: String) -> String {
+
+ // 第一种：brute force   超时
+ // On方 的时间复杂度找出所有子串，n 的时间复杂度 检查是否是回文
+
+/*
+class Solution {
+    func longestPalindrome(_ s: String) -> String {
+        guard s.count > 0 else { return "" }
+                
+        var res = ""
+        for i in 0..<s.count {
+            for j in i..<s.count {
+                if j - i + 1 > res.count {
+                    let range = s.index(s.startIndex, offsetBy: i) ... s.index(s.startIndex, offsetBy: j)
+                    let temp = String(s[range])
+                    if self.isPalindrome(temp) {
+                        res = temp
+                    }
+                }
+            }
+        }
+        
+        return res
+    }
     
-    
-    
-    return ""
+    func isPalindrome(_ s: String) -> Bool {
+        for i in 0..<(s.count/2 + 1) {
+//            print(s[s.index(s.startIndex, offsetBy: i)])
+            if s[s.index(s.startIndex, offsetBy: i)] != s[s.index(s.startIndex, offsetBy: s.count - i - 1)] {
+                return false
+            }
+        }
+        return true
+    }
 }
+*/
+
+
+
+class Solution {
+    func longestPalindrome(_ s: String) -> String {
+        
+        return ""
+    }
+}
+
+let s = Solution()
+print(s.longestPalindrome("a"))
+
+
+/*
+b
+ba
+bab
+baba
+babab
+bababd
+ 
+a
+ab
+aba
+abab
+ababd
+*/
+
