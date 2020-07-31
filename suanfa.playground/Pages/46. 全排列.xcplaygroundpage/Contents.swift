b83416dc3764c4e45f2e4a3d6aec0dee6c85adb2
lt <>
2020-07-31 14:40:02 +0800

    fix: 46

--- suanfa.playground/Pages/46. 全排列.xcplaygroundpage/Contents.swift
+++ suanfa.playground/Pages/46. 全排列.xcplaygroundpage/Contents.swift
@@ -18,18 +18,39 @@
  */
 
 class Solution {
+    
     var res: [[Int]] = []
 
     func permute(_ nums: [Int]) -> [[Int]] {
-
+        
+        var track: [Int] = []
+        var set =  Set<Int>()
+        
+        backtrack(nums, &set, &track)
         return res
     }
 
-    func backtrack() {
-
+    func backtrack(_ nums: [Int], _ set: inout Set<Int>, _ track: inout [Int] ) {
+        if track.count == nums.count {
+            res.append(track)
+            return
+        }
+        
+        for item in nums {
+            if set.contains(item) {
+                continue
+            }
+            set.insert(item)
+            track.append(item)
+            
+            backtrack(nums, &set, &track)
+            
+            set.remove(item)
+            track.removeLast()
+        }
     }
 }
 
 let s = Solution()
-print(s.permute([1,2,3]))
+print(s.permute([2,3,4]))
 
