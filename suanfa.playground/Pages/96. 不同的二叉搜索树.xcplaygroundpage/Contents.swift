4c53ed4703d3e31b9d89637c100460d37ad00352
lt <1004567344@qq.com>
2020-08-06 22:30:20 +0800

    feat: 96

---
+++ suanfa.playground/Pages/96. 不同的二叉搜索树.xcplaygroundpage/Contents.swift
@@ -0,0 +1,43 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/**
+ 给定一个整数 n，求以 1 ... n 为节点组成的二叉搜索树有多少种？
+
+ 示例:
+
+ 输入: 3
+ 输出: 5
+ 解释:
+ 给定 n = 3, 一共有 5 种不同结构的二叉搜索树:
+
+    1         3     3      2      1
+     \       /     /      / \      \
+      3     2     1      1   3      2
+     /     /       \                 \
+    2     1         2                 3
+
+ 链接：https://leetcode-cn.com/problems/unique-binary-search-trees
+ */
+
+class Solution {
+    func numTrees(_ n: Int) -> Int {
+        guard n > 0 else { return 0 }
+        guard n > 1 else { return 1 }
+
+        var res = Array.init(repeating: 0, count: n + 1)
+        res[0] = 1
+        res[1] = 1
+        for i in 2...n {
+            for j in 0..<i {
+                res[i] += res[j] * res[i - j - 1]
+            }
+        }
+        return res[n]
+    }
+}
+
+let s = Solution()
+print(s.numTrees(3))
+
